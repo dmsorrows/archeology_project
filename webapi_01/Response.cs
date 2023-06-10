f7e3531812eae14bc84790aaeea481be37b8f09a
Devin Sorrows <devinsorrows@gmail.com>
2023-06-09 20:11:55 -0500

    initial commit

---
+++ webapi_01/Response.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi
+{
+    public class Response
+    {
+        public string? Result { get; set; }
+        public string? Message { get; set; }
+        public List<Employee>? Employees { get; set; }
+    }
+}
